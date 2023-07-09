639b40f4b3b626a7ea868b8cd7140d6c7c6b4b7a
bniyomukiz <b.niyomukiz@alustudent.com>
2023-07-09 14:06:14 -0700

    uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    if uppercase(str) >= 65 and uppercase(str) <= 90:
-        print('{}'.format(uppercase(str)))
+    """Print a string in uppercase."""
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("")
