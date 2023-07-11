ae1412bfbdb8b2e056a88a0f8db65ceb84254c70
bniyomukiz <b.niyomukiz@alustudent.com>
2023-07-11 10:12:29 -0700

    args

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-import sys
-
 if __name__ == "__main__":
-    num_args = len(sys.argv) - 1
-
-    print("arguments:", num_args, end="")
-    if num_args == 0:
-        print(".", end="")
-    print()
-
-    if num_args > 0:
-        for i, arg in enumerate(sys.argv[1:], start=1):
-            print(i, ":", arg)
+    """ the number of and list of arg"""
+    import sys
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
