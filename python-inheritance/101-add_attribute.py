37c658442baa28297b311d15851d06a3f7e8a437
bniyomukiz <b.niyomukiz@alustudent.com>
2023-07-28 13:08:40 -0700

    101

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -1,51 +1,10 @@
 #!/usr/bin/python3
+"""How far with comments """
 
-class MyClass:
-    """This is a sample class."""
-
-    def __init__(self, name):
-        """
-        Initialize a MyClass object.
-
-        Parameters:
-            name (str): The name of the object.
-        """
-        self.name = name
-
-    def greet(self):
-        """
-        Print a greeting message.
-
-        Returns:
-            str: A greeting message.
-        """
-        return f"Hello, {self.name}!"
 
 def add_attribute(obj, name, value):
-    """
-    Add a new attribute to an object if possible.
-
-    Parameters:
-        obj: The object to which the attribute will be added.
-        name (str): The name of the attribute.
-        value: The value of the attribute.
-
-    Raises:
-        TypeError: If the object cannot have a new attribute.
-    """
-    if hasattr(obj, name):
-        setattr(obj, name, value)
-    else:
+    """are you documented"""
+    if hasattr(obj, '__dict__') is False:
         raise TypeError("can't add new attribute")
 
-# Sample test cases
-if __name__ == "__main__":
-    mc = MyClass("John")
-    print(mc.greet())
-
-    try:
-        a = "My String"
-        add_attribute(a, "name", "Bob")
-        print(a.name)
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
+    setattr(obj, name, value)
