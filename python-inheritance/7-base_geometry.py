62585bc591cee10e0f8a3b0d3fe29b2692ace8e3
bniyomukiz <b.niyomukiz@alustudent.com>
2023-07-28 12:35:53 -0700

    done

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,32 +1,47 @@
 #!/usr/bin/python3
-"""
-    class BaseGeometry (based on 6-base_geometry.py).
-
-    Public instance method: def area(self):
-    that raises an Exception with the message
-    area() is not implemented
-    Public instance method:
-    def integer_validator(self, name, value):
-        that validates value:
-        you can assume name is always a string
-        if value is not an integer: raise a TypeError
-        exception, with the message <name> must be an integer
-        if value is less or equal to 0: raise a ValueError
-        exception with the message <name> must be greater
-        than 0
-"""
-
 
 class BaseGeometry:
-    """ BaseGeometry class empty """
+    """
+    Base class for geometry operations.
+    """
 
     def area(self):
-        """ raise an exception """
+        """
+        Calculate the area of the geometry.
+        Raises:
+            Exception: This method should be implemented in the subclass.
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """ validates value is an integer """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        elif value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+        """
+        Validate an integer value.
+
+        Parameters:
+            name (str): The name of the value.
+            value: The value to be validated.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than or equal to 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+# Sample test case
+if __name__ == "__main__":
+    try:
+        bg = BaseGeometry()
+        bg.area()
+    except Exception as e:
+        print(f"Exception: {e}")
+
+    try:
+        bg = BaseGeometry()
+        bg.integer_validator("width", 10)
+        bg.integer_validator("height", "hello")
+    except Exception as e:
+        print(f"Exception: {e}")
