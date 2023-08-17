8c09ea25c40391a197d9b42c9b8a1f19f95d3cf5
bniyomukiz <b.niyomukiz@alustudent.com>
2023-08-17 14:01:23 -0700

    done

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,61 +1,36 @@
 #!/usr/bin/python3
-"""
-    Insert here module comment
-
-
-    Write a function that divides all elements of a matrix.
-
-    Prototype: def matrix_divided(matrix, div):
-    matrix must be a list of lists of integers or floats,
-    otherwise raise a TypeError exception with the message
-    matrix must be a matrix (list of lists)
-    of integers/floats
-
-    Each row of the matrix must be of the same size,
-    otherwise raise a TypeError exception
-    with the message Each row of the matrix must have
-    the same size
-
-    div must be a number (integer or float),
-    otherwise raise a TypeError exception
-    with the message div must be a number
-
-    div can’t be equal to 0, otherwise raise a
-    ZeroDivisionError exception
-    with the message division by zero
-
-    All elements of the matrix should be divided by div,
-    rounded to 2 decimal places
-    Returns a new matrix
-    You are not allowed to import any module
-"""
+# 2-matrix_divided.py
+"""Defines a matrix division function."""
 
 
 def matrix_divided(matrix, div):
-    """ Divide a matrix by a number div """
-    list_error = "matrix must be a matrix (list of lists) of integers/floats"
-    len_error = "Each row of the matrix must have the same size"
-    div_int_error = "div must be a number"
-    div_zero_error = "division by zero"
-    new_matrix = []
-    new_list = []
-    if not matrix:
-        raise TypeError(list_error)
-    if type(div) is not int and type(div) is not float:
-        raise TypeError(div_int_error)
-    if div is 0:
-        raise ZeroDivisionError(div_zero_error)
-    longitud = len(matrix[0])
-    for lista in matrix:
-        if type(lista) is not list:
-            raise TypeError(list_error)
-        if len(lista) != longitud:
-            raise TypeError(len_error)
-        for item in lista:
-            if type(item) is not int and type(item) is not float:
-                raise TypeError(list_error)
-            num = item / div
-            new_list.append(round(num, 2))
-        new_matrix.append(new_list)
-        new_list = []
-    return new_matrix
+    """Divide all elements of a matrix.
+
+    Args:
+        matrix (list): A list of lists of ints or floats.
+        div (int/float): The divisor.
+    Raises:
+        TypeError: If the matrix contains non-numbers.
+        TypeError: If the matrix contains rows of different sizes.
+        TypeError: If div is not an int or float.
+        ZeroDivisionError: If div is 0.
+    Returns:
+        A new matrix representing the result of the division.
+    """
+    if (not isinstance(matrix, list) or matrix == [] or
+            not all(isinstance(row, list) for row in matrix) or
+            not all((isinstance(ele, int) or isinstance(ele, float))
+                    for ele in [num for row in matrix for num in row])):
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers/floats")
+
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, int) and not isinstance(div, float):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,31 +1,22 @@
 #!/usr/bin/python3
-"""
-    Write a function that prints a square with
-    the character #.
-
-    Prototype: def print_square(size):
-    size is the size length of the square
-
-    size must be an integer, otherwise raise a TypeError
-    exception with the message size must be an integer
-
-    if size is less than 0, raise a ValueError exception
-    with the message size must be >= 0
-
-    if size is a float and is less than 0,
-    raise a TypeError exception with the message size
-    must be an integer
-"""
+# 4-print_square.py
+"""Defines a square-printing function."""
 
 
 def print_square(size):
-    """ print a square """
-    int_error = "size must be an integer"
-    neg_error = "size must be >= 0"
-    if type(size) is not int:
-        raise TypeError(int_error)
+    """Print a square with the # character.
+
+    Args:
+        size (int): The height/width of the square.
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is < 0
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
     if size < 0:
-        raise ValueError(neg_error)
+        raise ValueError("size must be >= 0")
+
     for i in range(size):
-        print("#" * size, end="")
-        print()
+        [print("#", end="") for j in range(size)]
+        print("")
