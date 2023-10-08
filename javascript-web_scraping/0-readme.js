38247ff4b4e045231b373cc512b0b5ec6578cb3e
bniyomukiz <b.niyomukiz@alustudent.com>
2023-10-08 13:20:02 -0700

    done

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,20 +1,20 @@
 #!/usr/bin/node
+const fs = require('fs');
 
-	const fs = require('fs');
-
-	function callback (error, data) {
-		  if (error) {
-			      console.log(`${error}`);
-			    } else {
-				        console.log(`${data}`);
-				      }
-	}
-
-function readFile (file) {
-	  fs.readFile(file, callback);
+function readFile (filePath) {
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      console.error(`An error occurred while reading the file: ${err}`);
+    } else {
+      console.log(data);
+    }
+  });
 }
 
-if (process.argv.length === 3) {
-	  const file = process.argv[2];
-	  readFile(file);
+// Check if the file path is provided as an argument
+if (process.argv.length > 2) {
+  const filePath = process.argv[2];
+  readFile(filePath);
+} else {
+  console.log('Please provide the file path as an argument.');
 }
